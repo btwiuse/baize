0985f912ddebe2958ae74f92a6c0872256fbc78b
navigaid <navigaid@gmail.com>
2022-07-12 18:18:35 +0800

    support `go install`
    go install -v github.com/btwiuse/baize@latest

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/spf13/cobra"
+
+	_ "net/http/pprof"
+
+	"github.com/btwiuse/baize/pkg/baize"
+	"github.com/btwiuse/baize/pkg/config"
+)
+
+func NewBazelExecutorCommand() *cobra.Command {
+	cmd := &cobra.Command{}
+	cfgPath := cmd.Flags().String("config", "config.toml", "config file to use")
+	cmd.RunE = func(cmd *cobra.Command, args []string) error {
+		cfg, err := config.NewConfigFromFile(*cfgPath)
+		if err != nil {
+			return err
+		}
+		s, err := baize.New(cfg)
+		if err != nil {
+			return err
+		}
+		if pprofAddr := cfg.GetExecutorConfig().PprofAddr; pprofAddr != "" {
+			go func() {
+				http.ListenAndServe(pprofAddr, nil)
+			}()
+		}
+		return s.Run()
+	}
+	return cmd
+}
+
+func main() {
+	err := NewBazelExecutorCommand().Execute()
+	if err != nil {
+		panic(err)
+	}
+}
